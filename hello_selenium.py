b8e8743a85a56ed24781b4aae07dd99f54f97682
fijiaaron <aarone@one-shore.com>
2021-10-01 15:45:49 -0600

    remove headless arguments from hello_selenium.py

--- hello_selenium.py
+++ hello_selenium.py
@@ -1,14 +1,10 @@
 from selenium import webdriver
 
-chrome_options = webdriver.ChromeOptions()
-
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-gpu")
-browser = webdriver.Chrome(options=chrome_options)
+browser = webdriver.Chrome()
 
 try:
-    browser.get("https://www.google.com")
-    print("Page title was '{}'".format(browser.title))
+	browser.get("https://www.google.com")
+	print("Page title was '{}'".format(browser.title))
 
 finally:
-    browser.quit()
+	browser.quit()
