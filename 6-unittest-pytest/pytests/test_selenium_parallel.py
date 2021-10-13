c7b54374c8beecaed158ce0a5fc7b0dfba260dfe
fijiaaron <aarone@one-shore.com>
2021-10-13 08:25:15 -0600

    update parallel tests

--- 6-unittest-pytest/pytests/test_selenium_parallel.py
+++ 6-unittest-pytest/pytests/test_selenium_parallel.py
@@ -1,4 +1,41 @@
-import pytset
+import pytest
 
 from selenium import webdriver
 
+import pytest
+from selenium import webdriver
+from time import sleep
+
+@pytest.mark.parametrize("browser", ["chrome", "firefox"])
+def test_open_google(browser):
+	if browser == "chrome":
+		driver = webdriver.Chrome()
+		driver.set_window_size(600, 600)
+		driver.set_window_position(0,0)
+	if browser == "firefox":
+		driver = webdriver.Firefox()
+		driver.set_window_size(600, 600)
+		driver.set_window_position(0,600)
+
+	driver.get("https://google.com")
+	print(driver.capabilities['browserName'], driver.title)
+
+	sleep(5)
+	driver.quit()
+
+@pytest.mark.parametrize("browser", ["chrome", "firefox"])
+def test_open_shop(browser):
+	if browser == "chrome":
+		driver = webdriver.Chrome()
+		driver.set_window_size(600, 600)
+		driver.set_window_position(600,0)
+	if browser == "firefox":
+		driver = webdriver.Firefox()
+		driver.set_window_size(600, 600)
+		driver.set_window_position(600,600)
+
+	driver.get("https://shop.one-shore.com")
+	print(driver.capabilities['browserName'], driver.title)
+
+	sleep(5)
+	driver.quit()
--- 6-unittest-pytest/pytests/test_selenium_parametrize.py
+++ 6-unittest-pytest/pytests/test_selenium_parametrize.py
@@ -1,13 +1,17 @@
 import pytest
 from selenium import webdriver
+from time import sleep
 
 @pytest.mark.parametrize("browser", ["chrome", "firefox"])
 def test_open_page(browser):
-    if browser == "chrome":
-        driver = webdriver.Chrome()
-    if browser == "firefox":
-        driver = webdriver.Firefox()
+	if browser == "chrome":
+		driver = webdriver.Chrome()
+	if browser == "firefox":
+		driver = webdriver.Firefox()
 
-    driver.get("https://google.com")
-    print( driver.capabilities['browserName'], driver.title)
-    driver.quit()
+	driver.maximize_window()
+	driver.get("https://google.com")
+	print(driver.capabilities['browserName'], driver.title)
+
+	sleep(3)
+	driver.quit()
