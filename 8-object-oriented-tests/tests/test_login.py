f66f78f21abed3b8168d8909ea9a9ff0223a8ed3
fijiaaron <aarone@one-shore.com>
2021-10-20 09:48:00 -0600

    add ini and fix try/finally error

--- 8-object-oriented-tests/tests/test_login.py
+++ 8-object-oriented-tests/tests/test_login.py
@@ -28,22 +28,30 @@ login_button = By.CSS_SELECTOR, "#login-form button#submit-login"
 
 @pytest.mark.success
 def test_login_success(driver, account):
+	print("__name__")
 	driver.get(login_url)
+	print("got title: " + driver.title)
 	assert driver.title == "Login"
 	print(account)
 	driver.find_element(*email_field_locator).send_keys(account.email)
 	driver.find_element(*password_field_locator).send_keys(account.password)
 	driver.find_element(*login_button).click()
+	print("got title: " + driver.title)
 	assert logged_in(driver)
 	assert get_account_name(driver) == account.full_name
 
+
 @pytest.mark.failure
 def test_login_failure(driver, account):
+	print("__name__")
 	driver.get(login_url)
 	assert driver.title == "Login"
+	print("got title: " + driver.title)
 	print(account)
 	driver.find_element(*email_field_locator).send_keys(account.email)
 	driver.find_element(*password_field_locator).send_keys("invalid password")
+	driver.find_element(*login_button).click()
+	print("got title: " + driver.title)
 	assert not logged_in(driver)
 
 def logged_in(driver):
@@ -51,15 +59,22 @@ def logged_in(driver):
 		wait = WebDriverWait(driver, 10)
 		wait.until(expected.title_is("ONESHORE DEMO SHOP"))
 		user_info = driver.find_element(By.CSS_SELECTOR, ".user-info")
+		print("user info: " + user_info.text)
 		if "Sign out" in user_info.text:
+			print("logged in")
 			return True
+		else:
+			return False
 	except TimeoutException as e: 
 		print("timeout")
 		print(e)
+		return False
 	except  WebDriverException as e:
 		print(e)
-	finally:
 		return False
+
 	
 def get_account_name(driver):
-	return driver.find_element(By.CSS_SELECTOR, ".user-info .account").text
+	account_name =  driver.find_element(By.CSS_SELECTOR, ".user-info .account").text
+	print(account_name)
+	return account_name
