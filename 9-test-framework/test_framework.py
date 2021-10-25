9e76a98ee225e4e139c2b628b0e54d37ff99ea0d
fijiaaron <aarone@one-shore.com>
2021-10-25 09:56:10 -0600

    add logging and pytest fixture

--- 9-test-framework/test_framework.py
+++ 9-test-framework/test_framework.py
@@ -1,11 +1,23 @@
+import sys
 import pytest
 import yaml
+import logging
 from time import sleep
 from selenium import webdriver
 
+logging.basicConfig()
+log = logging.getLogger("framework")
+log.setLevel(logging.DEBUG)
+log.addHandler(logging.FileHandler("framework.log"))
+log.addHandler(logging.StreamHandler(sys.stdout))
 
-def test_launch_browser():
+@pytest.fixture
+def driver():
 	driver = webdriver.Chrome()
-	driver.get("https://shop.one-shore.com")
+	yield driver
 	sleep(3)
 	driver.quit()
+
+def test_launch_browser(driver):
+	driver.get("https://shop.one-shore.com")
+	log.info(driver.title)
